9db73c4ccc84d1c0f89aae927a6e4fba32d0d7a9
Boris Z <Boris Z>
2022-09-19 14:52:08 +0300

    Calculation Project is done!

--- CalculationEnum/main.swift
+++ CalculationEnum/main.swift
@@ -2,10 +2,39 @@
 //  main.swift
 //  CalculationEnum
 //
-//  Created by Boris Alexandrov on 19.09.2022.
+//  Created by Boris Zitserman
 //
 
 import Foundation
 
-print("Hello, World!")
+enum CalculationType: String {
+    case subtraction = "вычитание"
+    case addition = "сложение"
+    case multiplication = "умножения"
+    case division = "деления"
+}
 
+func calculation(numOne: Int, numTwo: Int, calcType: CalculationType) -> Int? {
+    var res = 0
+    
+    switch calcType {
+    case .subtraction:
+        res = numOne - numTwo
+    case .addition:
+        res = numOne + numTwo
+    case .multiplication:
+        res = numOne * numTwo
+    case .division:
+        numTwo != 0 ? res = numOne / numTwo : print("На ноль не делим")
+        return nil
+    }
+    print("Результат \(calcType.rawValue) \(numOne) и \(numTwo) равен \(res)")
+    
+    return res
+
+}
+
+var result = calculation(numOne: 5, numTwo: 6, calcType: .division)
+result = calculation(numOne: 5, numTwo: 6, calcType: .multiplication)
+result = calculation(numOne: 5, numTwo: 6, calcType: .subtraction)
+result = calculation(numOne: 5, numTwo: 6, calcType: .addition)
